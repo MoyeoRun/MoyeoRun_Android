901aa1dade8a8457dd62f54f6dca5ff8f482a332
Kimchohee <chchgml10@gmail.com>
2022-03-17 22:05:07 +0900

    리뷰 반영 - themes-night.xml 에서 다크모드 미지원 설정 - 기본 테마 컬러 지정

--- app/src/main/java/com/moyerun/moyeorun_android/MoyeoRunApplication.kt
+++ app/src/main/java/com/moyerun/moyeorun_android/MoyeoRunApplication.kt
@@ -1,12 +1,10 @@
 package com.moyerun.moyeorun_android
 
 import android.app.Application
-import androidx.appcompat.app.AppCompatDelegate
 
 class MoyeoRunApplication: Application() {
 
     override fun onCreate() {
         super.onCreate()
-        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
     }
 }
