128da7a2b690872220ae8fc2daa58e8e6ddda882
winter223 <link5658@gmail.com>
2022-03-29 13:57:07 +0900

    [#8] 기본 다이얼로그 추가
    - Content 로 텍스트만을 보여주는 TextConfirmDialogFragment 추가
    - Fragment 를 띄우기 위한 확장함수 추가

--- app/src/main/java/com/moyerun/moyeorun_android/common/extension/FragmentExtension.kt
+++ app/src/main/java/com/moyerun/moyeorun_android/common/extension/FragmentExtension.kt
@@ -2,7 +2,9 @@ package com.moyerun.moyeorun_android.common.extension
 
 import android.widget.Toast
 import androidx.core.os.bundleOf
+import androidx.fragment.app.DialogFragment
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
 
 fun Fragment.toast(msg: String, isShort: Boolean = false) {
     Toast.makeText(context, msg, if (isShort) Toast.LENGTH_SHORT else Toast.LENGTH_LONG).show()
@@ -36,4 +38,16 @@ fun Fragment.putBooleanToArgument(key: String, value: Boolean) {
 
 fun Fragment.getBooleanFromArgument(key: String, value: Boolean): Boolean {
     return arguments?.getBoolean(key, value)?: value
+}
+
+inline fun FragmentManager?.showAllowingStateLoss(
+    tag: String?,
+    dialogFragmentFactory: () -> DialogFragment
+) {
+    if (this == null || isDestroyed) {
+        return
+    }
+    val transaction = beginTransaction()
+    transaction.add(dialogFragmentFactory(), tag)
+    transaction.commitAllowingStateLoss()
 }
