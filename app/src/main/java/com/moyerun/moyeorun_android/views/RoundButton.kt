0a84c3c1fb3eb295af041ce77bfa50985205e92c
Ju YunGyeom <30407907+gemiJ@users.noreply.github.com>
2022-04-14 03:05:21 +0900

    [#22] RectangleButton, RoundButton Style 적용

--- app/src/main/java/com/moyerun/moyeorun_android/views/RoundButton.kt
+++ app/src/main/java/com/moyerun/moyeorun_android/views/RoundButton.kt
@@ -2,19 +2,11 @@ package com.moyerun.moyeorun_android.views
 
 import android.content.Context
 import android.util.AttributeSet
-import androidx.appcompat.content.res.AppCompatResources
+import android.view.ContextThemeWrapper
 import androidx.appcompat.widget.AppCompatButton
-import androidx.core.content.ContextCompat
 import com.moyerun.moyeorun_android.R
 
 class RoundButton constructor(
     context: Context,
-    attributeSet: AttributeSet
-) : AppCompatButton(context, attributeSet) {
-
-    init {
-        this.background = AppCompatResources.getDrawable(context, R.drawable.shape_round_botton)
-        this.setTextColor(ContextCompat.getColor(context, R.color.main_white))
-        this.textSize = 18F
-    }
-}
+    attrs: AttributeSet? = null
+) : AppCompatButton(ContextThemeWrapper(context, R.style.RoundButtonTheme), attrs)
