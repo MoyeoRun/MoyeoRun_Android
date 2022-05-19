6639e52ec7ffe6d90e1098bfd7afa0d7803e518a
Kimchohee <chchgml10@gmail.com>
2022-05-19 22:02:45 +0900

    [#35] Jetpack Navigation 베이스 코드 작성 - 바텀 네비게이션 각 아이템에 매칭될 프래그먼트 생성 - navController 설정

---
+++ app/src/main/java/com/moyerun/moyeorun_android/record/RecordFragment.kt
@@ -0,0 +1,13 @@
+package com.moyerun.moyeorun_android.record
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.View
+import com.moyerun.moyeorun_android.R
+
+class RecordFragment : Fragment(R.layout.fragment_record) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
