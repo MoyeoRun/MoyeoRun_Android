6424263257a6294d9553d1bc76f123c35620aef7
winter223 <link5658@gmail.com>
2022-08-05 00:06:47 +0900

    [#17] 통신을 위한 네트워크 관련 베이스 코드 작성 - 컨버터 추가

--- app/src/main/java/com/moyerun/moyeorun_android/network/client/Retrofit.kt
+++ app/src/main/java/com/moyerun/moyeorun_android/network/client/Retrofit.kt
@@ -1,17 +1,17 @@
 package com.moyerun.moyeorun_android.network.client
 
 import com.moyerun.moyeorun_android.BuildConfig
-import com.moyerun.moyeorun_android.network.calladapter.ApiResultCallAdapterFactory
+import com.moyerun.moyeorun_android.network.MoyeorunJsonConverterFactory
 import com.moyerun.moyeorun_android.network.api.ApiService
+import com.moyerun.moyeorun_android.network.calladapter.ApiResultCallAdapterFactory
 import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
 
 private const val BASE_URL = BuildConfig.BASE_URL
 
 val retrofit: Retrofit = Retrofit.Builder()
     .baseUrl(BASE_URL)
     .addCallAdapterFactory(ApiResultCallAdapterFactory())
-    .addConverterFactory(GsonConverterFactory.create())
+    .addConverterFactory(MoyeorunJsonConverterFactory.create())
     .build()
 
 val apiService: ApiService = retrofit.create(ApiService::class.java)
